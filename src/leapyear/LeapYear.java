56ca303b599c8e556a22d8ff4abcb457b405b95b
Guruprasath-K <guruprasath.kann@gmail.com>
2023-05-21 13:16:49 +0530

    first commit

---
+++ src/leapyear/LeapYear.java
@@ -0,0 +1,18 @@
+package leapyear;
+
+
+public class LeapYear {
+
+    
+    public static void main(String[] args) {
+        
+        int n = 2005;
+        if (n%4==0) {
+            System.out.println("The given year is a Leap Year");
+        } else {
+            System.out.println("The given year is not a Leap Year");
+        }
+        
+    }
+    
+}
